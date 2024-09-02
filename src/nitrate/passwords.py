9f5dced6063e48719c6c24b68b9116d5af5093e1
Alex Chan <alex@alexwlchan.net>
2024-09-02 08:21:48 +0100

    Add docstrings for `passwords.py` and `test_passwords.py`

--- src/nitrate/passwords.py
+++ src/nitrate/passwords.py
@@ -35,6 +35,13 @@ def get_required_password(service_name: str, username: str) -> str:
 
 
 def use_in_memory_keyring(initial_passwords: dict[tuple[str, str], str]) -> None:
+    """
+    Create an in-memory keyring with a given set of passwords.
+
+    This is useful for testing, when you want to test the interaction
+    between a piece of code and the keychain.
+    """
+
     class InMemoryKeyring(keyring.backend.KeyringBackend):
         """
         A keyring implementation which stores passwords in a dictionary.
@@ -43,25 +50,42 @@ def use_in_memory_keyring(initial_passwords: dict[tuple[str, str], str]) -> None
         """
 
         def __init__(self) -> None:
+            """
+            Create an empty keychain.
+            """
             self.passwords: dict[tuple[str, str], str] = {}
 
         @property
         def priority(self) -> int:  # type: ignore
+            """
+            This is a required property on ``KeyringBackend`` implementations,
+            and is used to decide which backend to use.
+            """
             # We set a very high priority, so when this backend is used,
             # it will supersede any others.
             return 1_000_000  # pragma: no cover
 
         def set_password(self, service_name: str, username: str, password: str) -> None:
+            """
+            Store a password in the keychain.
+            """
             self.passwords[(service_name, username)] = password
 
         def get_password(self, service_name: str, username: str) -> str | None:
+            """
+            Retrieve a password from the keychain.
+            """
             return self.passwords.get((service_name, username))
 
-        # This function isn't currently used as part of the tests, but we
-        # need it to construct an instance of KeyringBackend.
         def delete_password(
             self, service_name: str, username: str
         ) -> None:  # pragma: no cover
+            """
+            Remove a password from the keychain.
+
+            This function isn't currently used as part of the tests, but
+            we need it to construct an instance of KeyringBackend.
+            """
             del self.passwords[(service_name, username)]
 
     kr = InMemoryKeyring()
