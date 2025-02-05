5367ec6410e3191727817c218f4c0e18fba047b5
Alex Chan <alex@alexwlchan.net>
2025-02-05 13:28:37 +0000

    Add a test that JSON-decoding remains backward compatible

--- tests/test_json.py
+++ tests/test_json.py
@@ -30,9 +30,33 @@ def test_can_json_round_trip(value: typing.Any) -> None:
     with ``NitrateDecoder`` and get the same value back.
     """
     json_string = json.dumps(value, cls=NitrateEncoder)
-    parsed_json_string = json.loads(json_string, cls=NitrateDecoder)
+    parsed_json_value = json.loads(json_string, cls=NitrateDecoder)
 
-    assert parsed_json_string == value
+    assert parsed_json_value == value
+
+
+@pytest.mark.parametrize(
+    ["json_string", "value"],
+    [
+        (
+            '{"type": "datetime.datetime", "value": "2001-02-03T04:05:06"}',
+            datetime.datetime(2001, 2, 3, 4, 5, 6),
+        ),
+        (
+            '{"type": "datetime.date", "value": "2001-02-03"}',
+            datetime.date(2001, 2, 3),
+        ),
+    ],
+)
+def test_can_decode_json(json_string: str, value: typing.Any) -> None:
+    """
+    We can decode JSON strings created with ``NitrateEncoder``.
+
+    Note: we use hard-coded strings here to ensure the decoder remains
+    compatible across versions, e.g. something encoded with nitrate 1.1
+    can be decoded with nitrate 1.2.
+    """
+    assert json.loads(json_string, cls=NitrateDecoder) == value
 
 
 def test_an_unrecognised_type_still_fails() -> None:
