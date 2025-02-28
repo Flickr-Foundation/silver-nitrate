a30b864dd9946cef9b654e02712422f9caa5ade0
Alex Chan <alex@alexwlchan.net>
2025-02-28 15:05:48 +0000

    Add a vanilla implementation of Flickr login

--- tests/stubs/vcr.pyi
+++ tests/stubs/vcr.pyi
@@ -6,4 +6,6 @@ def use_cassette(
     cassette_name: str,
     cassette_library_dir: str,
     decode_compressed_response: bool,
+    filter_query_parameters: list[tuple[str, str]] | None = None,
+    filter_headers: list[tuple[str, str]] | None = None,
 ) -> contextlib.AbstractContextManager[Cassette]: ...
